80f6036db770e5738ef4fbe83a9a5ecad67074f9
tungmt <tungmt@outlook.com>
2021-06-11 10:29:00 +0700

    fix 1688 issue

--- src/Global.js
+++ src/Global.js
@@ -4,71 +4,75 @@
 */
 
 import {
-    Dimensions,
-    Platform,
-    StatusBar
-  } from 'react-native'
-  
-  import media from './Media';
+  Dimensions,
+  Platform,
+  StatusBar
+} from 'react-native'
 
-  export function isIphoneX() {
-    const dimen = Dimensions.get('window');
-    return (
-        Platform.OS === 'ios' &&
-        !Platform.isPad &&
-        !Platform.isTVOS &&
-        ((dimen.height === 812 || dimen.width === 812) || (dimen.height === 896 || dimen.width === 896))
-    );
-  }
-  
-  export function ifIphoneX(iphoneXStyle, regularStyle) {
-    if (isIphoneX()) {
-        return iphoneXStyle;
-    }
-    return regularStyle;
-  }
-  
-  export function getStatusBarHeight(safe) {
-    return Platform.select({
-        ios: ifIphoneX(safe ? 44 : 30, 20),
-        android: StatusBar.currentHeight
-    });
-  }
-  
-  export function getBottomSpace() {
-    return isIphoneX() ? 34 : 0;
-  }
+import media from './Media';
 
-  export function imageUrl(image_url){
-    if(image_url.indexOf('https:') == -1){
-      return 'https:' + image_url
-    }
+export function isIphoneX() {
+  const dimen = Dimensions.get('window');
+  return (
+    Platform.OS === 'ios' &&
+    !Platform.isPad &&
+    !Platform.isTVOS &&
+    ((dimen.height === 780 || dimen.width === 780)
+      || (dimen.height === 812 || dimen.width === 812)
+      || (dimen.height === 844 || dimen.width === 844)
+      || (dimen.height === 896 || dimen.width === 896)
+      || (dimen.height === 926 || dimen.width === 926))
+  );
+}
 
-    return image_url
+export function ifIphoneX(iphoneXStyle, regularStyle) {
+  if (isIphoneX()) {
+    return iphoneXStyle;
   }
+  return regularStyle;
+}
 
-  export const convertMoney = (money) => {
-    if(!money) return money
-    return (Math.round(money * 100) / 100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+export function getStatusBarHeight(safe) {
+  return Platform.select({
+    ios: ifIphoneX(safe ? 44 : 30, 20),
+    android: StatusBar.currentHeight
+  });
+}
+
+export function getBottomSpace() {
+  return isIphoneX() ? 34 : 0;
+}
+
+export function imageUrl(image_url) {
+  if (image_url.indexOf('https:') == -1) {
+    return 'https:' + image_url
   }
 
-  export const contacts = [
-    {title: 'Hotline', number: '1900989977', action: 'tel:1900989977', icon: 'phone'},
-    {title: 'Zalo', number: '+840393382222', action: 'https://zalo.me/1807717681848049893', icon: 'headset'},
-    {title: 'Messenger', number: 'MaiDzo.vn', action: 'http://m.me/MaiDzo.vn', icon: 'facebook-messenger'},
-  ]
+  return image_url
+}
 
-  export default Global = {
-    MainColor: '#EE4D2D',
-    ScreenWidth : Dimensions.get('screen').width,
-    ScreenHeight : Dimensions.get('screen').height,
-    FontName : 'Arial',
-    apiUrl: 'https://quanly.maidzo.vn/',
-    username: '',
-    userId: '',
-    userToken: '',
-    pushToken: '',
-    overlayPopView: null,
-  }
-  
-  export const Media = media;
+export const convertMoney = (money) => {
+  if (!money) return money
+  return (Math.round(money * 100) / 100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+}
+
+export const contacts = [
+  { title: 'Hotline', number: '1900989977', action: 'tel:1900989977', icon: 'phone' },
+  { title: 'Zalo', number: '+840393382222', action: 'https://zalo.me/1807717681848049893', icon: 'headset' },
+  { title: 'Messenger', number: 'MaiDzo.vn', action: 'http://m.me/MaiDzo.vn', icon: 'facebook-messenger' },
+]
+
+export default Global = {
+  MainColor: '#EE4D2D',
+  ScreenWidth: Dimensions.get('screen').width,
+  ScreenHeight: Dimensions.get('screen').height,
+  FontName: 'Arial',
+  apiUrl: 'https://quanly.maidzo.vn/',
+  username: '',
+  userId: '',
+  userToken: '',
+  pushToken: '',
+  overlayPopView: null,
+}
+
+export const Media = media;
