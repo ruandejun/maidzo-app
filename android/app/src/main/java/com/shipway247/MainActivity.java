6d8cb6f565c87ad0f596a2bdb1d9772387c4b70c
Tung Mai <tungmt@outlook.com>
2023-11-16 23:13:11 +0700

    fix android

--- android/app/src/main/java/com/shipway247/MainActivity.java
+++ android/app/src/main/java/com/shipway247/MainActivity.java
@@ -3,7 +3,6 @@ package com.shipway247;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,14 +14,4 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Shipway247";
     }
-
-      @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-            return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-                       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-                      }
-    };
-          }
 }
